58ce02388e231dbd91b0b9156206a86745c4d41f
Jonashio <jonashio.es@gmail.com>
2023-11-27 10:16:39 +0100

    Initial Commit

---
+++ RandomUser/RandomUserApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUserApp.swift
+//  RandomUser
+//
+//  Created by Jonashio on 27/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RandomUserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
