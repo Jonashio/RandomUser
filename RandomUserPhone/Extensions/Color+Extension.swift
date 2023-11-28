ef955bca6794225e0510fc13273d4426152efaa5
Jonashio <jonashio.es@gmail.com>
2023-11-29 00:28:23 +0100

    Features: Create SplashView Add Dependencies with SPM Create LoadingView Create bridge for use Lottie Prepare previews data for design Adapt the usecase

---
+++ RandomUserPhone/Extensions/Color+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  Color+Extension.swift
+//  RandomUserPhone
+//
+//  Created by Jonashio on 28/11/23.
+//
+
+import Foundation
---
+++ RandomUserPhone/Source/Presentation/Splash/View/SplashView.swift
@@ -0,0 +1,8 @@
+//
+//  SplashView.swift
+//  RandomUserPhone
+//
+//  Created by Jonashio on 28/11/23.
+//
+
+import Foundation
